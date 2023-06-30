7058de996834ca91a4a35820415212c59047135f
unknown <joaquinmutolo@gmail.com>
2023-06-30 12:28:18 -0300

    cors fix

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const {dbConnection} = require('./src/db/config');
+const cors = require('cors');
 
 const app = express();
 dbConnection();
 
+app.use(cors());
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+//
+app.use((req, res, next) => {
+  res.setHeader('Referrer-Policy', 'same-origin');
+  next();
+});
+//
 
 app.get("/", (req, res) => {
     res.status(200).send({ message: "You are connected to the project" });
